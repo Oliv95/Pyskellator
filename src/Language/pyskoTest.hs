6bb5ed6419dc06cf67852809cb3586ddf5e9d6f6
axel olivecrona <axel.olivecrona@1995@gmail.com>
2017-04-07 14:04:58 +0200

    integer constants now tranformed in lambda functions

--- src/Language/pyskoTest.hs
+++ src/Language/pyskoTest.hs
@@ -5,14 +5,29 @@ unModule :: ModuleSpan -> [Statement SrcSpan]
 unModule (Module xs) = xs
 
 -- Gets the SrcSpan list from python code
-f = unModule $ fst $ (\(Right x) -> x) (parseModule a "")
+f a = unModule $ fst $ (\(Right x) -> x) (parseModule a "")
+fb = fst $ (\(Right x) -> x) (parseModule a "")
 
-a = "x = \"Hello\""
+a = "x = 5\ny=8"
 
-changeAssignment (Assign x _ c) = Assign x (Int 5 "5" c ) c
+changeAssignment (Assign x expr c) = Assign x (exprToLambda expr) c
 changeAssignment              a = a
 
-doIt = pretty (Module (fmap changeAssignment f))
+doIt a = pretty (Module (fmap changeAssignment (f a)))
 
 main :: IO()
 main = undefined
+
+exprToLambda e@(Int value lit a) = Call (Paren (Lambda [] e a) a) [] a
+exprToLambda e                   = e
+
+splitAssign (Assign t e a) = (t,e)
+
+idLambda = Lambda [Param  (Ident "x" "") Nothing Nothing ""] (Var (Ident "x" "") "") ""
+
+
+
+
+
+
+fromRight (Right a) = a
