0c2c9ed01d7e0eaf3e3abd2056d1f2152ca2d7c9
axel olivecrona <axel.olivecrona@1995@gmail.com>
2017-04-15 19:13:30 +0200

    added a bit of skeleton code

--- src/Language/pyskoTest.hs
+++ src/Language/pyskoTest.hs
@@ -1,8 +1,74 @@
 import Language.Python.Common
 import Language.Python.Version3.Parser
 
+stmListFromSource :: String -> String -> ModuleSpan
+stmListFromSource scoure fileName = fst $ (\(Right x) -> x) (parseModule scoure fileName)
+
 unModule :: ModuleSpan -> [Statement SrcSpan] 
 unModule (Module xs) = xs
+
+todo = error "Not implemented"
+
+transformStatement :: Statement t -> Statement t
+transformStatement (Import imorts annot)                   = todo
+transformStatement (FromImport from imports annot)         = todo
+transformStatement (While expr body whileElse annot)       = todo
+transformStatement (For vars generator body forElse annot) = todo
+transformStatement (Fun name params resAnnot body annot)   = todo
+transformStatement (Class name args body annot)            = todo
+transformStatement (Conditional condGuards condElse annot) = todo
+transformStatement (Assign target expr annot)              = todo
+transformStatement (AugmentedAssign target op expr annot)  = todo
+transformStatement (Decorated decorators definition annot) = todo
+transformStatement (Return expr annot)                     = todo
+transformStatement (Try try except tryElse finally annot)  = todo
+transformStatement (Raise expr annot)                      = todo
+transformStatement (With context body annot)               = todo
+transformStatement (Pass annot)                            = todo
+transformStatement (Break annot)                           = todo
+transformStatement (Continue annot)                        = todo
+transformStatement (Delete exprs annot)                    = todo
+transformStatement (StmtExpr expr annot)                   = todo
+transformStatement (Global vars annot)                     = todo
+transformStatement (NonLocal vars annot)                   = todo
+transformStatement (Assert exprs annot)                    = todo
+
+transformExpression :: Expr t -> Expr t
+transformExpression (Var indent annot)                           = todo
+transformExpression (Int value lit annot)                        = todo
+transformExpression (Float value lit annot)                      = todo
+transformExpression (Imaginary value lit annot)                  = todo
+transformExpression (Bool value annot)                           = todo
+transformExpression (None annot)                                 = todo
+transformExpression (Ellipsis annot)                             = todo
+transformExpression (ByteStrings strings annot)                  = todo
+transformExpression (Strings strings annot)                      = todo
+transformExpression (Call expr args annot)                       = todo
+transformExpression (Subscript target index annot)               = todo
+transformExpression (SlicedExpr traget slices annot)             = todo
+transformExpression (CondExpr trueBranch cond falseBranch annot) = todo
+transformExpression (BinaryOp op left right annot)               = todo
+transformExpression (UnaryOp op arg annot)                       = todo
+transformExpression (Dot expr attribute annot)                   = todo
+transformExpression (Lambda params body annot)                   = todo
+transformExpression (Tuple exprs annot)                          = todo
+transformExpression (Yield arg annot)                            = todo
+transformExpression (Generator comprehension  annot)             = todo
+transformExpression (ListComp  comprehension annot)              = todo
+transformExpression (List list annot)                            = todo
+transformExpression (Dictionary mappings annot)                  = todo
+transformExpression (DictComp comprehension annot)               = todo
+transformExpression (Set exprs annot)                            = todo
+transformExpression (SetComp comprehension annot)                = todo
+transformExpression (Starred expr annot)                         = todo
+transformExpression (Paren expr annot)                           = todo
+
+transformSlice :: Slice t -> Slice t
+transformSlice (SliceProper lower upper stride annot) = todo
+transformSlice (SliceExpr expr annot)                 = todo
+transformSlice (SliceEllipsis annot)                  = todo
+
+
 
 -- Gets the SrcSpan list from python code
 f a = unModule $ fst $ (\(Right x) -> x) (parseModule a "")
