e8553372123516dcb87173446ade15cdda3e9b62
axel olivecrona <axel.olivecrona@1995@gmail.com>
2017-05-25 11:33:08 +0200

    bla

--- src/Main.hs
+++ src/Main.hs
@@ -5,12 +5,14 @@ import System.Environment
 
 main :: IO()
 main = do
+    -- Mer av detta borde ligga i obfuscator känns det som
     args <- getArgs
     let inFile = head args
     let outFile = head (tail args)
     source <- readFile inFile
     let stms = stmListFromSource source
     obfuAst  <- mapM transformStatement stms
+    --pretty känns rätt sketchy
     let code = unlines (fmap show (fmap pretty obfuAst))
     writeFile outFile code
     
